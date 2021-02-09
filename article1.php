a08cb257dd38e8c9178d6be4c4f30e4967e0e983
VivianeSc <gavroche_191@yahoo.co.uk>
2021-02-09 12:58:56 +0100

    requete pour recuperer les articles dans la bdd

--- article1.php
+++ article1.php
@@ -1,1 +1,12 @@
-La loi primitive
+<?php
+try {
+    $bdd = new PDO('mysql:host=localhost;dbname=jean-forteroche;charset=utf8', 'root', 'root');
+} catch (Exception $e) {
+    die('Erreur : ' . $e->getMessage());
+}
+$reponse = $bdd->query('SELECT title, content FROM billets WHERE id=1');
+while ($donnees = $reponse->fetch()) {
+    echo $donnees['title']  . '<br />' .  $donnees['content'];
+}
+$reponse->closeCursor();
+?>
