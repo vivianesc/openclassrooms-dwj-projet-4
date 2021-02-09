6e2edb9f98131087c516be4086633f854410497d
VivianeSc <gavroche_191@yahoo.co.uk>
2021-02-09 13:02:54 +0100

    requete vers la bdd pour recuperer les articles

--- index.php
+++ index.php
@@ -40,40 +40,51 @@
     <section id="billets">
         <div class="heading_description">
             <h2>LES BILLETS</h2>
-            <p>bla bla</p>
         </div>
         <div class="display-posts-listing image-left">
             <div class="listing-item">
-                <a class="image" href="article1.php"><img width="150" height="150" src="images/billet-2.jpg"
-                        class="thumbnail" alt="Billet 1" /></a>
-                <a class="title" href="article1.php">La loi primitive</a> <span class="excerpt-dash">-</span>
-                <span class="excerpt">Un soir, que le juge présidait une réunion et que ses fils étaient absorbés par le
-                    règlement d’un nouveau club athlétique,
-                    le traître Manoël appelle doucement Buck, qui le suit sans défiance, convaincu qu’il s’agit d’une
-                    simple promenade à la brume.
-                    Tous deux traversent sans encombre la propriété, gagnent la grande route et arrivent tranquillement
-                    à la petite gare de Collège-Park. Là, un homme inconnu&hellip;
-                    <span class="excerpt-more"></span>
-                </span>
+                <a class="image" href="article1.php"><img width="150" height="150" src="images/billet-2.jpg" class="thumbnail" alt="Billet 1" /></a>
+                <div class="excerpt">
+                    <?php
+                    try {
+                        $bdd = new PDO('mysql:host=localhost;dbname=jean-forteroche;charset=utf8', 'root', 'root');
+                    } catch (Exception $e) {
+                        die('Erreur : ' . $e->getMessage());
+                    }
+                    $reponse = $bdd->query('SELECT title, content FROM billets WHERE id=1');
+                    while ($donnees = $reponse->fetch()) {
+                        echo $donnees['title']  . '<br />' .  $donnees['content'];
+                    }
+                    $reponse->closeCursor();
+                    ?>
+                    <a class="title" href="article1.php">Lire la suite&hellip;</a> 
+                </div>
+                <p><a href="article1.php" class="button">Lire l'article</a></p>
             </div>
+
             <div class="listing-item">
-                <a class="image" href="article2.php"><img width="150" height="150" src="images/billet-4.jpg"
-                        class="thumbnail" alt="Billet 2" /></a>
-                <a class="title" href="article2.php">La loi du bâton et de la dent</a> <span class="excerpt-dash">-</span>
-                <span class="excerpt">La première journée de Buck sur la grève de Dyea fut un véritable cauchemar. 
-                    Toutes les heures lui apportaient une émotion ou une surprise. Brutalement arraché à sa vie paresseuse et ensoleillée, 
-                    il se voyait sans transition rejeté du cœur de la civilisation au centre même de la barbarie. Ici, ni paix, ni repos, 
-                    ni sécurité ; tout était confusion, choc et péril, de là, nécessité absolue d’être toujours en éveil, car les bêtes 
-                    et les hommes ne reconnaissaient que la loi du bâton et de la dent. Des chiens innombrables couvraient cette terre nouvelle, 
-                    et Buck n’avait jamais rien vu de semblable aux batailles que se livraient ces animaux, pareils à des loups ; 
-                    son premier contact avec eux lui resta à jamais dans la mémoire. L’expérience ne lui fut pas&hellip;
-                    <span class="excerpt-more"></span>
-                </span>
+                <a class="image" href="article2.php"><img width="150" height="150" src="images/billet-4.jpg" class="thumbnail" alt="Billet 2" /></a>
+                <div class="excerpt">
+                <?php
+                    try {
+                        $bdd = new PDO('mysql:host=localhost;dbname=jean-forteroche;charset=utf8', 'root', 'root');
+                    } catch (Exception $e) {
+                        die('Erreur : ' . $e->getMessage());
+                    }
+                    $reponse = $bdd->query('SELECT title, content FROM billets WHERE id=2');
+                    while ($donnees = $reponse->fetch()) {
+                        echo $donnees['title']  . '<br />' .  $donnees['content'];
+                    }
+                    $reponse->closeCursor();
+                    ?>
+                    <a class="title" href="article1.php">Lire la suite&hellip;</a>
+                </div>
+                <p><a href="article2.php" class="button">Lire l'article</a></p>
             </div>
         </div>
     </section>
     <!--/Section content end-->
-    
+
     <!-- /footer -->
     <footer>
         <div class="heading_description">
