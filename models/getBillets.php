7aedd0175aad58c65b7948d314caa42f7ac9a771
VivianeSc <gavroche_191@yahoo.co.uk>
2021-02-28 18:16:49 +0100

    MVC des billets

---
+++ models/getBillets.php
@@ -0,0 +1,15 @@
+<?php
+function getBillets()
+{
+try {
+    $bdd = new PDO('mysql:host=localhost;dbname=jean-forteroche;charset=utf8', 'root', 'root');
+} catch (Exception $e) {
+    die('Erreur : ' . $e->getMessage());
+}
+$req = $bdd->query('SELECT id, title, content, DATE_FORMAT(date_creation, \'%d/%m/%Y \') AS date_creation_fr FROM billets ORDER BY date_creation DESC LIMIT 0, 10');
+
+return $req;
+}
+?>
+
+//require('index_billets_view.php');
--- pages/index.php
+++ pages/index.php
@@ -1,10 +1,19 @@
 <?php
-// Récupération des données
-try {
-    $bdd = new PDO('mysql:host=localhost;dbname=jean-forteroche;charset=utf8', 'root', 'root');
-} catch (Exception $e) {
-    die('Erreur : ' . $e->getMessage());
-}
-$req = $bdd->query('SELECT id, title, content, DATE_FORMAT(date_creation, \'%d/%m/%Y \') AS date_creation_fr FROM billets ORDER BY date_creation DESC LIMIT 0, 10');
+require('../models/getBillets.php');
 
-require('../models/index_billets_view.php');
+$req = getBillets();
+
+require('viewBillets.php');
+?>
+
+
+//<?php
+// Récupération des données
+//try {
+ //   $bdd = new PDO('mysql:host=localhost;dbname=jean-forteroche;charset=utf8', 'root', 'root');
+//} catch (Exception $e) {
+//    die('Erreur : ' . $e->getMessage());
+//}
+//$req = $bdd->query('SELECT id, title, content, DATE_FORMAT(date_creation, \'%d/%m/%Y \') AS date_creation_fr FROM billets ORDER BY date_creation DESC LIMIT 0, 10');
+
+//require('index_billes
