5dd6a4be07f4f8f77eb256bf2de1ce83f39cee85
VivianeSc <gavroche_191@yahoo.co.uk>
2021-03-04 14:12:47 +0100

    suppression balises php

--- modules/login_error.php
+++ modules/login_error.php
@@ -1,5 +1,2 @@
-<?php
-echo '<h2>Mauvais identifiant ou mot de passe !</h2>
-    <p><a href="login.php" class="login_button">Retour page login</a></p>';
-?>
-                
+<h2>Mauvais identifiant ou mot de passe !</h2>
+<p><a href="login.php" class="login_button">Retour page login</a></p>
