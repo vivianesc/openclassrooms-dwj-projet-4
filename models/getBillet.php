645f5e295020036a9597f52ecd924b53070f8aec
VivianeSc <gavroche_191@yahoo.co.uk>
2021-03-20 16:12:29 +0100

    model pour recuperer billet et commentaires

--- models/getBillet.php
+++ models/getBillet.php
@@ -10,3 +10,12 @@ function getBillet($billetId)
 
     return $billet;
 }
+
+function getComments($billetId)
+{
+    $db = dbConnect();
+    $req = $db->prepare('SELECT pseudo, comment, DATE_FORMAT(date_comment, \'%d/%m/%Y \') AS date_comment_fr FROM comments WHERE id_billet = ? ORDER BY date_comment');
+    $req->execute(array($billetId));
+
+    return $req;
+}
