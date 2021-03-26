b111bd2d2f7956ad8a73904817159ca6349f9cbf
VivianeSc <gavroche_191@yahoo.co.uk>
2021-03-26 15:06:51 +0100

    module comment devient module commentForm

---
+++ modules/commentForm.php
@@ -0,0 +1,10 @@
+<form action="../pages/comment.php?billet=1" method="post">
+    <fieldset id="">
+        <legend>AJOUTER UN COMMENTAIRE</legend>
+        <p>
+            <label for="pseudo">Pseudo</label> : <input type="text" name="pseudo" id="pseudo" /><br />
+            <textarea name="comment" id="comment" rows="8" cols="45">Votre commentaire</textarea>
+            <input type="submit" value="Envoyer" class="login_button" />
+        </p>
+    </fieldset>
+</form>
