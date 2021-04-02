822ffd1ed4465f77cd982314937b6016fa0b0684
VivianeSc <gavroche_191@yahoo.co.uk>
2021-04-02 14:17:34 +0200

    on inscrit le contenu html dans echo

--- pages/administration.php
+++ pages/administration.php
@@ -1,26 +1,28 @@
-
 <?php $title = 'Jean Forteroche, billet'; ?>
 
 <?php ob_start(); ?>
 
-    <section id="introduction_administration">
-        <div class="heading_description">
-            <?php
-            if (isset($_POST['username']) and $_POST['username'] ==  "Jean" and isset($_POST['password']) and $_POST['password'] ==  "eden") {
-                echo '<h2>Bonjour Jean, bienvenue dans votre éditeur de billets</h2>
-                <div class="cpanel">
+<section id="introduction_administration">
+    <div class="heading_description">
+        <?php
+        if (isset($_POST['username']) and $_POST['username'] ==  "Jean" and isset($_POST['password']) and $_POST['password'] ==  "eden") { ?>
+            <h2>Bonjour Jean, bienvenue dans votre éditeur de billets</h2>
+            <div class="cpanel">
                 <ul>
-                <li><a href="billet_ajouter.php" class="button">Ajouter un article</a></li>
-                <li><a href="billet_editer.php" class="button">Liste des commentaires signalés</a></li>
+                    <li><a href="billet_ajouter.php" class="button">Ajouter un article</a></li>
+                    <li><a href="billet_editer.php" class="button">Liste des commentaires signalés</a></li>
                 </ul>
-                </div>';
-            } else {
-                require('../modules/login_error.php');
-            }
-            ?>
-    </section>
-    <!--/Section introduction end -->
+            </div>
 
-    <?php $content = ob_get_clean(); ?>
+        <?php
+            //<p>require('../modules/billetForm.php');</p>
+        } else {
+            require('../modules/login_error.php');
+        }
+        ?>
+</section>
+<!--/Section introduction end -->
 
-    <?php require('../templates/page.php'); ?>
+<?php $content = ob_get_clean(); ?>
+
+<?php require('../templates/page.php'); ?>
