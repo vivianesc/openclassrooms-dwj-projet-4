6f75e41ed475f88912adb4897ad5a207edec60f5
VivianeSc <gavroche_191@yahoo.co.uk>
2021-04-07 19:26:05 +0200

    creation page message commentaire envoyé

---
+++ views/commentView.php
@@ -0,0 +1,16 @@
+<?php $title = 'Jean Forteroche, commentaire'; ?>
+
+<?php ob_start(); ?>
+
+<section id="introduction_administration">
+    <div class="heading_description">
+        <h2>Votre commentaire a été ajouté</h2>
+        <h3><a href="../pages/index.php">Retour à la liste des billets</a></h3>
+
+    </div>
+
+</section>
+
+<?php $content = ob_get_clean(); ?>
+
+<?php require('../templates/page.php'); ?>
