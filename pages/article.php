186733d323adfa0faec9596a4e1cb73096139f98
VivianeSc <gavroche_191@yahoo.co.uk>
2021-04-28 19:31:47 +0200

    ajout template

--- pages/article.php
+++ pages/article.php
@@ -1,11 +1,11 @@
 <?php
 require('../models/billet.php');
-require('../models/comment.php');
 
 if (isset($_GET['billet']) && $_GET['billet'] > 0) {
     $billet = getBillet($_GET['billet']);
     $comments = getComments($_GET['billet']);
     require('../views/billetView.php');
+    
 } else {
     echo 'Erreur : aucun identifiant de billet envoyé';
 }
--- views/createBilletView.php
+++ views/createBilletView.php
@@ -1,12 +1,23 @@
-<form method="post" action="../pages/administration.php?login=true">
-    <p>
-    <fieldset id="">
-        <legend>CRÉATION DE BILLET</legend>
-        <p>
-            <input type="text" name="title" placeholder="Titre" required><br />
-            <textarea name="content" placeholder="Contenu" rows="20" cols="100" required></textarea><br />
-            <input type="submit" value="Ajouter" class="login_button">
-        </p>
-    </fieldset>
-    </p>
+<?php $title = 'Jean Forteroche, billet'; ?>
+
+<?php ob_start(); ?>
+
+<section id="introduction_administration">
+    <div class="heading_description">
+        <form method="post" action="../pages/administration.php?login=true">
+            <p>
+            <fieldset id="">
+                <legend>CRÉATION DE BILLET</legend>
+                <p>
+                    <input type="text" name="title" placeholder="Titre" required><br />
+                    <textarea name="content" placeholder="Contenu" rows="20" cols="100" required></textarea><br />
+                    <input type="submit" value="Ajouter" class="login_button">
+                </p>
+                <p><a href="" class="button">Liste des commentaires signalés</a></p>
+</section>
+</fieldset>
 </form>
+
+<?php $content = ob_get_clean(); ?>
+
+<?php require('../templates/page.php'); ?>
